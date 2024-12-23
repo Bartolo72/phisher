7707cfee2772bfc9ac8b46b7982ec56ef1c16b5e
Bartolo72 <bartosz.kosinski.b@gmail.com>
2024-12-23 13:22:12 +0100

    added first notebook

--- module/phisher_module.py
+++ module/phisher_module.py
@@ -14,6 +14,11 @@ class PhisherhModule(pl.LightningModule):
         self.model = model
         self.optimizer = optimizer
 
+        self.accuracy = torchmetrics.Accuracy(task="multiclass", num_classes=self.num_classes)
+        self.f1 = torchmetrics.F1Score(task="multiclass", num_classes=self.num_classes)
+        self.precision = torchmetrics.Precision(task="multiclass", num_classes=self.num_classes)
+        self.recall = torchmetrics.Recall(task="multiclass", num_classes=self.num_classes)
+
 
     def forward(self, x):
         return self.model(x)
@@ -33,28 +38,38 @@ class PhisherhModule(pl.LightningModule):
     def common_test_valid_step(self, batch, batch_idx):
         loss, outputs, y = self.common_step(batch, batch_idx)
         preds = torch.argmax(outputs, dim=1)
-        acc = torchmetrics.functional.accuracy(preds, y, num_classes = self.num_classes, task="multiclass")
-        return loss, acc
+
+        acc = self.accuracy(preds, y)
+        f1 = self.f1(preds, y)
+        precision = self.precision(preds, y)
+        recall = self.recall(preds, y)
+        return loss, acc, f1, precision, recall
 
 
     def training_step(self, batch, batch_idx):
-        loss, acc = self.common_test_valid_step(batch, batch_idx)
+        loss, acc, _, __, ___ = self.common_test_valid_step(batch, batch_idx)
         self.log('train_loss', loss, on_step=True, on_epoch=True, logger=True)
         self.log('train_acc', acc, on_step=True, on_epoch=True, logger=True)
         return loss
 
 
     def validation_step(self, batch, batch_idx):
-        loss, acc = self.common_test_valid_step(batch, batch_idx)
+        loss, acc, f1, precision, recall = self.common_test_valid_step(batch, batch_idx)
         self.log('val_loss', loss, prog_bar=True)
         self.log('val_acc', acc, prog_bar=True)
+        self.log('val_f1', f1, prog_bar=True)
+        self.log('val_precision', precision, prog_bar=True)
+        self.log('val_recall', recall, prog_bar=True)
         return loss
 
 
     def test_step(self, batch, batch_idx):
-        loss, acc = self.common_test_valid_step(batch, batch_idx)
+        loss, acc, f1, precision, recall = self.common_test_valid_step(batch, batch_idx)
         self.log('test_loss', loss, prog_bar=True)
         self.log('test_acc', acc, prog_bar=True)
+        self.log('test_f1', f1, prog_bar=True)
+        self.log('test_precision', precision, prog_bar=True)
+        self.log('test_recall', recall, prog_bar=True)
         return loss
 
 
