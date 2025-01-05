34b464e0eeaaca6c991f114660f9d04084c5ea90
Bartolo72 <bartosz.kosinski.b@gmail.com>
2025-01-05 01:06:45 +0100

    loading model from ckpt

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ from phisher.dataset.utils import prepare_phish_dataset
 
 console = Console()
 
+
 @hydra.main(config_path="conf", config_name="config", version_base="1.1")
 def main(cfg: DictConfig) -> None:
     console.log("[bold gold3]Starting dataset preparation...[/bold gold3]")
-    
+
     with Progress(
         SpinnerColumn(spinner_name="dots", style="cyan"),
         TextColumn("[progress.description]{task.description}"),
@@ -32,43 +33,58 @@ def main(cfg: DictConfig) -> None:
             prepare_phish_dataset(target_file_path="phish_dataset.csv", **data_config)
             progress.update(task, advance=1)
     console.log("[bold green]Dataset prepared successfully.[/bold green]")
-    
+
     console.log("[bold gold3]Initializing components...[/bold gold3]")
 
     dataset: PhishingDataset = instantiate(cfg.dataset)
     console.log(f"Dataset instantiated: [green]{type(dataset).__name__}[/green]")
-    
+
     data_module: PhishingDataModule = instantiate(cfg.data_module, dataset=dataset)
     data_module.setup(**cfg.data_module_setup)
     console.log(f"DataModule instantiated: [green]{type(data_module).__name__}[/green]")
-    
+
     model: PhisherModel = instantiate(cfg.model)
     console.log(f"Model instantiated: [green]{type(model).__name__}[/green]")
-    
-    optimizer: torch.optim.Optimizer = instantiate(cfg.optimizer, params=model.parameters())
-    console.log(f"Optimizer instantiated: [green]{type(optimizer).__name__}[/green]")
-    
-    module = PhisherhModule(model, optimizer, num_classes=2)
-    console.log(f"Lightning Module instantiated: [green]{type(module).__name__}[/green]")
-    
-    callbacks: List[Callback] = [instantiate(cb_cfg) for cb_cfg in cfg.callbacks.values()]
+
+    has_ckpt: bool = "model_ckpt" in cfg and cfg.model_ckpt
+    if has_ckpt:
+        console.log(
+            f"[bold gold3]Loading model from checkpoint: {cfg.model_ckpt}[/bold gold3]"
+        )
+        module = PhisherhModule.load_from_checkpoint(cfg.model_ckpt, model=model)
+    else:
+        optimizer: torch.optim.Optimizer = instantiate(
+            cfg.optimizer, params=model.parameters()
+        )
+        console.log(
+            f"Optimizer instantiated: [green]{type(optimizer).__name__}[/green]"
+        )
+        module = PhisherhModule(model, optimizer)
+        console.log(
+            f"Lightning Module instantiated: [green]{type(module).__name__}[/green]"
+        )
+
+    callbacks: List[Callback] = [
+        instantiate(cb_cfg) for cb_cfg in cfg.callbacks.values()
+    ]
     console.log(f"Callbacks: [green]{[type(cb).__name__ for cb in callbacks]}[/green]")
-    
+
     logger = instantiate(cfg.logger)
     console.log(f"Logger: [green]{type(logger).__name__}[/green]")
-    
+
     trainer: pl.Trainer = pl.Trainer(
         **cfg.trainer,
         logger=logger,
         callbacks=callbacks,
     )
-    console.log("[bold gold3]Training started...[/bold gold3]")
-    trainer.fit(module, data_module)
-    console.log("[bold gold3]Training finished.[/bold gold3]")
+
+    if not has_ckpt:
+        console.log("[bold gold3]Training started...[/bold gold3]")
+        trainer.fit(module, data_module)
+        console.log("[bold gold3]Training finished.[/bold gold3]")
+
     trainer.test(model=module, datamodule=data_module)
     console.log("[bold gold3]Testing finished.[/bold gold3]")
-
-    torch.save(module.state_dict(), "weihts_pth_phisher_one_hot_module_filtered.pth")
 
 
 if __name__ == "__main__":
--- phisher/module/phisher_module.py
+++ phisher/module/phisher_module.py
@@ -5,11 +5,10 @@ import pytorch_lightning as pl
 
 
 class PhisherhModule(pl.LightningModule):
-    def __init__(self, model, optimizer, num_classes, learning_rate=1e-3):
+    def __init__(self, model, optimizer, learning_rate=1e-3):
         super().__init__()
         self.save_hyperparameters()
         self.learning_rate = learning_rate
-        self.num_classes = num_classes
         self.model = model
         self.optimizer = optimizer
 
@@ -46,12 +45,8 @@ class PhisherhModule(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         loss, acc, _, __, ___, auc = self.common_test_valid_step(batch, batch_idx)
-        self.log(
-            "train_loss", loss, on_step=True, on_epoch=True, logger=True, prog_bar=True
-        )
-        self.log(
-            "train_acc", acc, on_step=True, on_epoch=True, logger=True, prog_bar=True
-        )
+        self.log("train_loss", loss, on_step=True, on_epoch=True, logger=True)
+        self.log("train_acc", acc, on_step=True, on_epoch=True, logger=True)
         self.log("train_auc", auc, on_step=True, on_epoch=False, logger=True)
         return loss
 
